a25d5b2bda8c180956ac4ad586f360732ef9996a
Hassan Ayman <hsankraz66@gmail.com>
2023-06-24 11:41:09 +0300

    Revert "this is the test commit"
    This reverts commit f74287be7523914f769543dd5af6dacd74108b56.

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,5 @@ void main() {
   //runApp(const MyApp());
   print('this is the first commit');
   print('this is the second commit');
-  print('lksdjlkflkjgxcvkljvcxjkl');
+  print('this is the third commit');
 }
